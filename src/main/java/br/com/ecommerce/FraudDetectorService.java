b448318ea3f2075a7b5cf9d37c6fc2cc91b05106
Marcos Duarte <marcosduarte1994@gmail.com>
2023-06-26 16:44:30 -0300

    chore: changes

--- src/main/java/br/com/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/ecommerce/FraudDetectorService.java
@@ -1,16 +1,18 @@
 package br.com.ecommerce;
+import java.util.Map;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDetectorService {
   public static void main(String[] args) {
     var fraudeService = new FraudDetectorService();
-    try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(), 
-        "ECOMMERCE_NEW_ORDER", fraudeService::parse)) {
+    try(var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), 
+        "ECOMMERCE_NEW_ORDER", fraudeService::parse, Order.class, Map.of())) {
       service.run();
   }
 }
   
-  private void parse(ConsumerRecord<String, String> record) {
+  private void parse(ConsumerRecord<String, Order> record) {
      System.out.println("----------------------------------------------");
           System.out.println("Processing new order, checking for fraud.");
           System.out.println(record.key());
